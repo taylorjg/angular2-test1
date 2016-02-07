aa667306b1021f8103f02024d7a45669ab2cab41
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-02-07 10:28:14 +0000

    Added gulpfile and moved client files into client folder

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require("gulp");
+var ts = require('gulp-typescript');
+var del = require('del');
+
+var PATHS = {
+    libs: [
+        'node_modules/angular2/bundles/angular2.dev.js',
+        'node_modules/angular2/bundles/http.dev.js',
+        'node_modules/angular2/bundles/router.dev.js',
+        'node_modules/systemjs/dist/system.src.js'
+    ],
+    client: {
+        ts: ["client/app/**/*.ts"],
+        html: "client/**/*.html"
+    },
+    dist: "dist",
+    distClient: "dist/client",
+    distLib: "dist/lib"
+};
+
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("build", ["ts", "html", "libs"], function () {
+    console.log("Inside build gulp task");
+});
+
+gulp.task("clean", function (done) {
+    console.log("Inside clean gulp task");
+    del([PATHS.dist], done);
+});
+
+gulp.task("ts", function () {
+    console.log("Inside ts gulp task");
+    return gulp
+        .src(PATHS.client.ts)
+        .pipe(ts(tsProject))
+        .pipe(gulp.dest(PATHS.distClient));
+});
+
+gulp.task("html", function () {
+    console.log("Inside html gulp task");
+    return gulp
+        .src(PATHS.client.html)
+        .pipe(gulp.dest(PATHS.distClient));
+});
+
+gulp.task("libs", function () {
+    console.log("Inside libs gulp task");
+    return gulp
+        .src(PATHS.libs)
+        .pipe(gulp.dest(PATHS.distLib));
+});
+
+gulp.task("default", ["build"], function () {
+    console.log("Inside default gulp task");
+});
